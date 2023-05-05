97be193514d849b7dffd5a7ce3708e5e1b4f18df
Luis Henrique <65178969+Louiixx-h@users.noreply.github.com>
2023-05-04 22:48:30 -0300

    files moved to main folder

--- Assets/_Game/Src/DataDialog.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "UI/Dialog")]
-public class DataDialog : ScriptableObject
-{
-    public string title;
-    public string message;
-}
--- Assets/_Game/Src/EnumEventAction.cs
+++
@@ -1,5 +0,0 @@
-public enum EnumEventAction
-{
-    EVENT_BOSS_FIGHT_SHADOW,
-    EVENT_CAMERA,
-}
