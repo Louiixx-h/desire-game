4d1f44fa0678680170970f12547c9051dac1458e
Luis Henrique <65178969+Louiixx-h@users.noreply.github.com>
2023-05-04 22:15:38 -0300

    refactoring in movement behaviour

--- Assets/_Game/Src/Game/Behaviours/Movement.cs
+++ Assets/_Game/Src/Game/Behaviours/Movement.cs
@@ -1,18 +1,20 @@
 using UnityEngine;
 
-namespace _Game.Src.Game.Behaviours
+namespace Desire.Game.Behaviours
 {
     public class Movement
     {
         private readonly Rigidbody2D _rigidbody;
-        private readonly float _movementSpeed;
         private readonly SpriteRenderer _sprite;
+        private readonly float _movementSpeed;
+        private readonly float _jumpForce;
         
-        public Movement(SpriteRenderer sprite, float movementSpeed, Rigidbody2D rigidbody)
+        public Movement(SpriteRenderer sprite, float movementSpeed, Rigidbody2D rigidbody, float jumpForce)
         {
             _rigidbody = rigidbody;
             _movementSpeed = movementSpeed;
             _sprite = sprite;
+            _jumpForce = jumpForce;
         }
 
         public void Tick(float deltaTime)
@@ -22,16 +24,19 @@ namespace _Game.Src.Game.Behaviours
         
         public void Tick(float deltaTime, Vector2 motion)
         {
-            _rigidbody.velocity = new Vector2(motion.x * _movementSpeed * deltaTime, _rigidbody.velocity.y);
+            _rigidbody.velocity = new Vector2(motion.x * _movementSpeed, _rigidbody.velocity.y);
 
-            if (motion.x > 0)
+            _sprite.flipX = motion.x switch
             {
-                _sprite.flipX = true;
-            }
-            if (motion.x < 0)
-            {
-                _sprite.flipX = false;
-            }
+                > 0 => true,
+                < 0 => false,
+                _ => _sprite.flipX
+            };
+        }
+
+        public void Jump()
+        {
+            _rigidbody.AddForce(Vector2.up * _jumpForce);
         }
     }
 }
