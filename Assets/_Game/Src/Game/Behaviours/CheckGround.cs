c48bebb4675f34633c495518f69abecff047d359
Luis Henrique <65178969+Louiixx-h@users.noreply.github.com>
2023-05-04 22:15:59 -0300

    checkground behaviour created

---
+++ Assets/_Game/Src/Game/Behaviours/CheckGround.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace Desire.Game.Behaviours
+{
+    public class CheckGround
+    {
+        private readonly Transform _groundPosition;
+        private readonly float _radius;
+        private readonly LayerMask _groundLayer;
+        
+        public CheckGround(Transform groundPosition, float radius, LayerMask groundLayer)
+        {
+            _groundPosition = groundPosition;
+            _radius = radius;
+            _groundLayer = groundLayer;
+        }
+        
+        public bool IsGrounded()
+        {
+            return Physics2D.OverlapCircle(_groundPosition.position, _radius, _groundLayer);
+        }
+    }
+}
