7f03bd399a07a6847619a7c41ddad468601251ee
Luis Henrique <65178969+Louiixx-h@users.noreply.github.com>
2023-05-07 12:34:44 -0300

    class removed

--- Assets/_Game/Src/main/DataLayers.cs
+++
@@ -1,8 +0,0 @@
-public struct DataLayers
-{
-    public const int GROUND = 3;
-    public const int PLAYER = 8;
-    public const int ENEMY = 7;
-    public const int LIMBO = 9;
-    public const int LADDER = 6;
-}
