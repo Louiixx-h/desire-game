b6e66e53ee494925cf69dfb6ae0034384ee5575b
Luis Henrique <65178969+Louiixx-h@users.noreply.github.com>
2023-05-07 12:35:07 -0300

    refactoring ui

--- Assets/_Game/Src/main/Ui/UiManager.cs
+++
@@ -1,9 +0,0 @@
-public class UiManager : GameManager<UiManager>
-{
-    public UiInteraction uiInteraction;
-    public UiStatusPlayer uiStatusPlayer;
-    public UiStatusBoss uiStatusBoss;
-    public UiGameOver uiGameOver;
-    public UiMsg uiMsg;
-    public UiDialog uiDialog;
-}
