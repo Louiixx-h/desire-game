5c70f9cc43521e87b6d04c27aa7fa8280eda1d98
Luis Henrique <65178969+Louiixx-h@users.noreply.github.com>
2023-05-07 12:37:23 -0300

    AI updated

--- Assets/_Game/Src/main/Game/AI/AIBoss.cs
+++ Assets/_Game/Src/main/Game/AI/AIBoss.cs
@@ -1,145 +1,149 @@
-using System.Collections;
-using UnityEngine;
+ using System.Collections;
+ using Desire.Game.Behaviours;
+ using UnityEngine;
 
-public class AIBoss : MonoBehaviour, IDamageable
-{
-    [SerializeField] SpriteRenderer spriteRenderer;
-    [SerializeField] DoDamage doDamage;
-    [SerializeField] ParticleSystem particleDeath;
-    [SerializeField] [Range(1, 20)] float moveSpeed;
-    [SerializeField] [Range(1, 60)] float damage = 30;
+ namespace Desire.Game.AI
+ {
+     public class AIBoss : MonoBehaviour, IDamageable
+     {
+         [SerializeField] SpriteRenderer spriteRenderer;
+         [SerializeField] ParticleSystem particleDeath;
+         [SerializeField] [Range(1, 20)] float moveSpeed;
+         [SerializeField] [Range(1, 60)] float damage = 30;
 
-    [HideInInspector] public delegate void OnDeathDelegate();
-    [HideInInspector] public event OnDeathDelegate OnDeath;
+         [HideInInspector]
+         public delegate void OnDeathDelegate();
 
-    float maxLife = 100;
-    float currentLife = 0;
-    float takeHitCooldown = 0.2f;
+         [HideInInspector] public event OnDeathDelegate OnDeath;
 
-    bool isDeath = false;
-    bool isAttacking = false;
-    bool isRunning = false;
-    bool canTakeHit = true;
-    bool isWaiting = true;
+         float maxLife = 100;
+         float currentLife = 0;
+         float takeHitCooldown = 0.2f;
 
-    Animator    animator;
-    Rigidbody2D rb;
-    GameObject  target;
+         bool isDeath = false;
+         bool isAttacking = false;
+         bool isRunning = false;
+         bool canTakeHit = true;
+         bool isWaiting = true;
 
-    Vector3 currentPosition;
-    Vector3 targetPosition;
+         Animator animator;
+         Rigidbody2D rb;
+         GameObject target;
 
-    void Start()
-    {
-        animator = GetComponent<Animator>();
-        rb = GetComponent<Rigidbody2D>();
+         Vector3 currentPosition;
+         Vector3 targetPosition;
 
-        target = GameObject.FindWithTag("Player");
-        HealthManager(maxLife);
-        UiManager.Instance.uiStatusBoss.Show();
-    }
+         void Start()
+         {
+             animator = GetComponent<Animator>();
+             rb = GetComponent<Rigidbody2D>();
 
-    void FixedUpdate()
-    {
-        if (currentLife <= 0)
-        {
-            DeathState();
-            return;
-        }
-        if (isDeath || isWaiting) return;
-        currentPosition = transform.position;
-        targetPosition = target.transform.position;
+             target = GameObject.FindWithTag("Player");
+             HealthManager(maxLife);
+         }
 
-        if (Vector2.Distance(currentPosition, targetPosition) <= 1.5f)
-        {
-            AttackState();
-            return;
-        }
-        isAttacking = false;
-        FollowState();
-    }
+         void FixedUpdate()
+         {
+             if (currentLife <= 0)
+             {
+                 DeathState();
+                 return;
+             }
 
-    public void Fight()
-    {
-        isWaiting = false;
-    }
+             if (isDeath || isWaiting) return;
+             currentPosition = transform.position;
+             targetPosition = target.transform.position;
 
-    void FollowState()
-    {
-        if (!isRunning)
-        {
-            animator.Play("Run");
-            isRunning = true;
-        }
-        var direction = (currentPosition - targetPosition).normalized * -1;
+             if (Vector2.Distance(currentPosition, targetPosition) <= 1.5f)
+             {
+                 AttackState();
+                 return;
+             }
 
-        rb.MovePosition(
-            rb.transform.position +
-            direction *
-            moveSpeed *
-            Time.deltaTime
-        );
+             isAttacking = false;
+             FollowState();
+         }
 
-        if (direction.x > 0) transform.localScale = new Vector3(1, 1, 1);
-        if (direction.x < 0) transform.localScale = new Vector3(-1, 1, 1);
-    }
+         public void Fight()
+         {
+             isWaiting = false;
+         }
 
-    void AttackState()
-    {
-        if (isAttacking) return;
-        isAttacking = true;
-        isRunning = false;
-        animator.Play("Attack1");
-    }
+         void FollowState()
+         {
+             if (!isRunning)
+             {
+                 animator.Play("Run");
+                 isRunning = true;
+             }
 
-    public void Attack()
-    {
-        doDamage.Attack(damage);
-    }
+             var direction = (currentPosition - targetPosition).normalized * -1;
 
-    public void EndAttack()
-    {
-        isAttacking = false;
-        animator.Play("Idle");
-    }
+             rb.MovePosition(
+                 rb.transform.position +
+                 direction *
+                 moveSpeed *
+                 Time.deltaTime
+             );
 
-    public void TakeDamage(float value)
-    {
-        if (isDeath & !canTakeHit) return;
-        HealthManager(value);
-    }
+             if (direction.x > 0) transform.localScale = new Vector3(1, 1, 1);
+             if (direction.x < 0) transform.localScale = new Vector3(-1, 1, 1);
+         }
 
-    void DeathState()
-    {
-        if (isDeath) return;
-        isDeath = true;
-        UiManager.Instance.uiStatusBoss.Hide();
-        rb.velocity = Vector2.zero;
-        animator.Play("Death");
-        particleDeath.gameObject.SetActive(true);
-        OnDeath.Invoke();
-        StartCoroutine(Disappear());
-    }
+         void AttackState()
+         {
+             if (isAttacking) return;
+             isAttacking = true;
+             isRunning = false;
+             animator.Play("Attack1");
+         }
 
-    IEnumerator Disappear()
-    {
-        yield return new WaitForSeconds(2f);
-        Destroy(gameObject);
-        yield return null;
-    }
+         public void Attack()
+         {
+         }
 
-    IEnumerator TakeHitCooldown()
-    {
-        yield return new WaitForSeconds(takeHitCooldown);
-        canTakeHit = true;
-        yield return null;
-    }
+         public void EndAttack()
+         {
+             isAttacking = false;
+             animator.Play("Idle");
+         }
 
-    public void HealthManager(float value)
-    {
-        currentLife += value / maxLife;
-        UiManager.Instance.uiStatusBoss.ChangeLife(currentLife);
-        canTakeHit = false;
-        StartCoroutine(TakeHitCooldown());
-    }
-}
+         public void TakeDamage(float value)
+         {
+             if (isDeath & !canTakeHit) return;
+             HealthManager(value);
+         }
+
+         void DeathState()
+         {
+             if (isDeath) return;
+             isDeath = true;
+             rb.velocity = Vector2.zero;
+             animator.Play("Death");
+             particleDeath.gameObject.SetActive(true);
+             OnDeath.Invoke();
+             StartCoroutine(Disappear());
+         }
+
+         IEnumerator Disappear()
+         {
+             yield return new WaitForSeconds(2f);
+             Destroy(gameObject);
+             yield return null;
+         }
+
+         IEnumerator TakeHitCooldown()
+         {
+             yield return new WaitForSeconds(takeHitCooldown);
+             canTakeHit = true;
+             yield return null;
+         }
+
+         public void HealthManager(float value)
+         {
+             currentLife += value / maxLife;
+             canTakeHit = false;
+             StartCoroutine(TakeHitCooldown());
+         }
+     }
+ }
