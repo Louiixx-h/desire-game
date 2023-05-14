f6db20b8875b1d55173aa04796765d7029916d15
Luis Henrique <65178969+Louiixx-h@users.noreply.github.com>
2023-05-14 18:26:11 -0300

    command added to Interactable

--- Assets/_Game/Src/main/Game/Behaviours/IInteractable.cs
+++ Assets/_Game/Src/main/Game/Behaviours/IInteractable.cs
@@ -1,11 +1,11 @@
-using System;
+using Desire.Game.Command;
 
 namespace Desire.Game.Behaviours
 {
     public interface IInteractable
     {
+        public PlayerTakeLifeCommand Command { get; set; }
         public void ShowInteraction();
         public void HideInteraction();
-        public void DoInteraction();
     }
 }
--- Assets/_Game/Src/main/Game/Behaviours/Interactable.cs
+++ Assets/_Game/Src/main/Game/Behaviours/Interactable.cs
@@ -1,71 +1,64 @@
+using Desire.Game.Command;
+using Desire.Game.Inputs;
 using UnityEngine;
-using Desire.Game.Player;
 using TMPro;
 
 namespace Desire.Game.Behaviours
 {
     public class Interactable : MonoBehaviour, IInteractable
     {
+        [SerializeField] private InputInteractableAction input;
         [SerializeField] private GameObject canvas;
         [SerializeField] private TextMeshProUGUI text;
         [SerializeField] private string message;
-        [SerializeField] private LayerMask target;
-
-        private PlayerBehaviour _player;
-        private ViewStateEnum _viewState;
+        [SerializeField] private string target;
         
-        enum ViewStateEnum
-        {
-            ShowView, HideView
-        }
+        [field:SerializeField] public PlayerTakeLifeCommand Command { get; set; }
         
         private void Start()
         {
             HideInteraction();
         }
+        
+        private void OnEnable()
+        {
+            input.OnAction += OnInputAction;
+        }
+
+        private void OnDisable()
+        {
+            input.OnAction -= OnInputAction;
+        }
+
+        private void OnInputAction(bool value)
+        {
+            Command.Execute();
+        }
 
         public void ShowInteraction()
         {
-            _viewState = ViewStateEnum.ShowView;
             canvas.SetActive(true);
             text.text = message;
         }
         
         public void HideInteraction()
         {
-            _viewState = ViewStateEnum.HideView;
             canvas.SetActive(false);
             text.text = "";
         }
 
-        public void DoInteraction()
-        {
-            print("interaction");
-        }
-
-        private void Update()
-        {
-            if(_player == null) return;
-            if(_viewState.Equals(ViewStateEnum.ShowView)) return;
-            if (!_player.IsAction) return;
-            
-            DoInteraction();
-        }
-
         private void OnTriggerEnter2D(Collider2D col)
         {
-            if(!col.gameObject.TryGetComponent(out PlayerBehaviour player)) return;
+            if(!col.gameObject.CompareTag(target)) return;
 
             ShowInteraction();
-            _player = player;
         }
 
         private void OnTriggerExit2D(Collider2D other)
         {
-            if(!other.gameObject.TryGetComponent(out PlayerBehaviour player)) return;
+            if(!other.gameObject.CompareTag(target)) return;
             
             HideInteraction();
-            _player = null;
         }
     }
 }
